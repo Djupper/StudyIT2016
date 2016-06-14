88aeba95ab9861518536af3b3c7230dab426bb78
Евгений <Djuuper@mail.ru>
2016-06-15 00:27:37 +0600

    14/06

---
+++ Test/src/etg/prog/test/test.java
@@ -0,0 +1,10 @@
+package etg.prog.test;
+
+/**
+ * Created by Djupp_000 on 14.06.2016.
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("Привет!");
+    }
+}
