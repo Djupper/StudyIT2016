79391dc5eeb3b3be3e2db78175af39a70ea2ee1e
Djupper <Djupper@mail.ru>
2016-06-14 10:32:39 +0700

    Yahoo!!!

---
+++ Test1/src/etg/prog/test1/Main.java
@@ -0,0 +1,7 @@
+package etg.prog.test1;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello!");
+    }
+}
---
+++ Test2/src/etg/prog/test2/Main.java
@@ -0,0 +1,8 @@
+package etg.prog.test2;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hi!");
+
+    }
+}
